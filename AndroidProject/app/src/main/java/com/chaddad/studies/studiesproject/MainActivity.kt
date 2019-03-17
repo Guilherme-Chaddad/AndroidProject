7c2a68a7b0f2b865db1040b8625ca3c8e46085ec
Guilherme Chaddad <guichaddad@gmail.com>
2019-03-17 00:15:29 -0300

    Creating navigation drawer

--- AndroidProject/app/src/main/java/com/chaddad/studies/studiesproject/MainActivity.kt
+++ AndroidProject/app/src/main/java/com/chaddad/studies/studiesproject/MainActivity.kt
@@ -3,9 +3,15 @@ package com.chaddad.studies.studiesproject
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBar
+import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +20,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val actionbar: ActionBar? = supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu_white)
+        }
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            drawerLayout.closeDrawers()
+
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+
+            true
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -22,19 +52,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(R.id.exit == item.itemId){
-            this.finishAndRemoveTask()
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            R.id.exit -> {
+                this.finishAndRemoveTask()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
-        return true
     }
 
     override fun onBackPressed() {
-        if(clickedBackTwice){
-            super.onBackPressed()
-            return
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+
+        if(drawerLayout.isDrawerOpen(navigationView)) {
+            drawerLayout.closeDrawers()
+        } else {
+            if (clickedBackTwice) {
+                super.onBackPressed()
+                return
+            }
+            clickedBackTwice = true
+            Toast.makeText(this, R.string.doubleClickToExitMessage, Toast.LENGTH_SHORT).show()
+            Handler().postDelayed(Runnable { clickedBackTwice = false }, 2000)
         }
-        clickedBackTwice = true
-        Toast.makeText(this, R.string.doubleClickToExitMessage, Toast.LENGTH_SHORT).show()
-        Handler().postDelayed(Runnable { clickedBackTwice = false }, 2000)
     }
 }
